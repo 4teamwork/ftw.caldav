faa346fe424ba42d86e653231ec92af057fe69e1
Jonas Baumann <jone@jone.ch>
2013-11-30 21:15:18 +0100

    Create package.

---
+++ README.rst
@@ -0,0 +1,24 @@
+ftw.caldav
+==========
+
+`ftw.caldav` provides `CalDAV` support for `Plone`_.
+
+
+
+Links
+-----
+
+- Github project: https://github.com/4teamwork/ftw.caldav
+- Issues: https://github.com/4teamwork/ftw.caldav/issues
+- Pypi: http://pypi.python.org/pypi/ftw.caldav
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.caldav
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.caldav`` is licensed under GNU General Public License, version 2.
+
+.. _Plone: http://www.plone.org/
---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.2.1.dev0'
+
+
+tests_require = [
+    'plone.app.testing',
+    'unittest2',
+    'zope.configuration',
+    ]
+
+
+setup(name='ftw.caldav',
+      version=version,
+      description='CalDAV support for Plone.',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw caldav plone',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.caldav',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
